3b8c4f9618ddbded69151128ad42c431f9676032
diegogarcia <consultor@dgpragmatic.com>
2019-08-20 20:48:09 -0500

    se adjuntan sonidos y código fuente

--- app/src/main/java/co/com/k4soft/migranja/MainActivity.java
+++ app/src/main/java/co/com/k4soft/migranja/MainActivity.java
@@ -1,7 +1,9 @@
 package co.com.k4soft.migranja;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playCerdo(View view) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.cerdo);
+        mediaPlayer.start();
+
+    }
+
+    public void playGato(View view) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.gato);
+        mediaPlayer.start();
     }
 }
